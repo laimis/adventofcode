7bfa84346eaf5fcb40128e29000a73a3c7ee9c23
unknown <laimis@gmail.com>
2022-12-08 11:03:53 -0800

    add day 6

---
+++ 2022/day6/Program.fs
@@ -0,0 +1,39 @@
+﻿open System.IO
+let input = File.ReadAllText "input.txt"
+
+let folder (row: int, found:bool) (chars: char array) =
+
+    if (found) then
+        (row, found)
+    else
+        let allUnique = chars |> Array.distinct |> Array.length = chars.Length
+
+        if allUnique then
+            (row, true)
+        else
+            (row + 1, false)
+
+let findCharacterOfInterest (input:string) windowSize =
+    let (windowIndex, found) =  
+        input.ToCharArray()
+        |> Array.windowed windowSize
+        |> Array.fold folder (0, false)
+
+    windowIndex + windowSize
+
+let startOfPacketWindowSize = 4
+let startOfPacket = findCharacterOfInterest input startOfPacketWindowSize
+printfn "Start of packet: %d" startOfPacket
+
+let startOfMessageWindowSize = 14
+let startOfMessage = findCharacterOfInterest input startOfMessageWindowSize
+printfn "Start of message: %d" startOfMessage
+
+// 0 - 4
+// 1 - 5
+// 2 - 6
+// 3 - 7
+// 4 - 8
+// 5 - 9
+// 6 - 10
+// 7 - 11
