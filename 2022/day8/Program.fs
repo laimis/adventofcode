2d18c9bc1c86e91797ed6e9fe78ea855f3c3e923
unknown <laimis@gmail.com>
2022-12-08 21:53:10 -0800

    part 2

--- 2022/day8/Program.fs
+++ 2022/day8/Program.fs
@@ -40,21 +40,59 @@ let checkIfVisible (grid:int[][]) (row:int) (col:int) =
 
         not isHiddenToLeft || not isHiddenToRight || not isHiddenUp || not isHiddenDown
 
-let iterateOverEachColumn rowIndex columnIndex value =
+let mapToVisibilityValue rowIndex columnIndex =
     let boolean = checkIfVisible grid rowIndex columnIndex
     match boolean with
     | true -> 1
     | false -> 0
 
-let answer =
-    grid
+let rec calculateScenicScore (grid:int[][]) (row:int) (col:int) (moveFunc) value score =
+    
+    let (newRow, newCol) = moveFunc(row, col)
+    if newCol <= 0 || newRow <= 0 then
+        score
+    elif newRow >= rows - 1 || newCol >= columns - 1 then
+        score
+    else
+        let toCompare = grid[newRow][newCol]
+        if toCompare >= value then
+            score
+        else
+            calculateScenicScore grid newRow newCol moveFunc value (score + 1)
+
+// grid 1 2 moveright 5 1
+//   newR: 1, newC: 3
+//   toCompare: 1
+//      grid 1 3 moveright 5 2
+//         newR: 1, newC: 4
+let mapToScenicScore rowIndex columnIndex =
+    let value = grid[rowIndex][columnIndex]
+
+    let upScore = calculateScenicScore grid rowIndex columnIndex (fun (row, col) -> (row-1, col)) value 1
+    let leftScore = calculateScenicScore grid rowIndex columnIndex (fun (row, col) -> (row, col-1)) value 1
+    let downScore = calculateScenicScore grid rowIndex columnIndex (fun (row, col) -> (row+1, col)) value 1
+    let rightScore = calculateScenicScore grid rowIndex columnIndex (fun (row, col) -> (row, col+1)) value 1
+    let finalScore = upScore * leftScore * downScore * rightScore
+    // printfn "(%d x %d) [%d]: up: %d, left: %d, down: %d, right: %d ==> %d" rowIndex columnIndex value upScore leftScore downScore rightScore finalScore
+    finalScore
+
+let transformArrayWithFunc func arr =
+    arr
     |> Array.mapi (fun rowIndex row ->
         row
         |> Array.mapi (fun columnIndex value ->
-            iterateOverEachColumn rowIndex columnIndex value
+            func rowIndex columnIndex
         )
     )
 
-let sum = answer |> Array.sumBy (fun x -> x |> Array.sumBy (fun y -> y))
+let folder (acc:int[]) (row:int[]) =
+    Array.concat [acc; row]
 
-printfn "sum: %d" sum
+let flatten arr =
+    arr |> Array.fold folder (array.Empty<int>())
+
+let sum = grid |> transformArrayWithFunc mapToVisibilityValue |> flatten |> Array.sum
+printfn "Number of visible trees: %d" sum
+
+let bestSpot = grid |> transformArrayWithFunc mapToScenicScore |> flatten |> Array.max
+printfn "Best spot's scenic score: %d" bestSpot
