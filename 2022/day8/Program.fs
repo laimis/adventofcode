59e74f3d19d1e6097a2596fbc8f77c7037e9f497
unknown <laimis@gmail.com>
2022-12-08 21:06:55 -0800

    get rid of 2d array

--- 2022/day8/Program.fs
+++ 2022/day8/Program.fs
@@ -4,42 +4,34 @@ let input = File.ReadAllLines("input.txt")
 let rows = input.Length
 let columns = input[0].Length
 
-// create a 2D array of ints
-let grid = Array2D.create rows columns 0
-
-printfn "rows: %d, columns: %d" rows columns
-
-// // initialize grid
-input 
-|> Array.iteri (fun row line -> 
-    line.ToCharArray()
-    |> Array.iteri (fun col ch ->
-        let num = int (ch.ToString())
-        Array2D.set grid row col num
+let grid =
+    input 
+    |> Array.map (fun line -> 
+        line.ToCharArray()
+        |> Array.map (fun c -> int c - 48)
     )
-)
 
-let rec checkIfHidden (grid:int[,]) (row:int) (col:int) (moveFunc) (value:int) =
+let rec checkIfHidden (grid:int[][]) (row:int) (col:int) (moveFunc) (value:int) =
     let (newRow, newCol) = moveFunc(row, col)
     if newCol < 0 || newRow < 0 then
         false
     elif newRow >= rows || newCol >= columns then
         false
     else
-        let toCompare = grid[newRow, newCol]
+        let toCompare = grid[newRow][newCol]
         if toCompare >= value then
             true
         else
             checkIfHidden grid newRow newCol moveFunc value
 
-let checkIfVisible (grid:int[,]) (row:int) (col:int) =
+let checkIfVisible (grid:int[][]) (row:int) (col:int) =
 
     if row = 0 then
         true
     elif col = 0 then
         true
     else
-        let value = grid.[row, col]
+        let value = grid[row][col]
 
         let isHiddenToLeft = checkIfHidden grid row col (fun (row, col) -> (row, col-1)) value
         let isHiddenToRight = checkIfHidden grid row col (fun (row, col) -> (row, col+1)) value
@@ -48,16 +40,21 @@ let checkIfVisible (grid:int[,]) (row:int) (col:int) =
 
         not isHiddenToLeft || not isHiddenToRight || not isHiddenUp || not isHiddenDown
 
-let mutable sum = 0
+let iterateOverEachColumn rowIndex columnIndex value =
+    let boolean = checkIfVisible grid rowIndex columnIndex
+    match boolean with
+    | true -> 1
+    | false -> 0
 
-grid
-    |> Array2D.iteri (fun row col value ->
-        let boolean = checkIfVisible grid row col
-        match boolean with
-        | true -> sum <- sum + 1
-        | false -> ()
-
-        printfn "row: %d, col: %d, value: %d, boolean: %b" row col value boolean
+let answer =
+    grid
+    |> Array.mapi (fun rowIndex row ->
+        row
+        |> Array.mapi (fun columnIndex value ->
+            iterateOverEachColumn rowIndex columnIndex value
+        )
     )
+
+let sum = answer |> Array.sumBy (fun x -> x |> Array.sumBy (fun y -> y))
 
 printfn "sum: %d" sum
