79544375db543a391679f5c28fdd9206832ad528
unknown <laimis@gmail.com>
2022-12-09 09:36:23 -0800

    implement day 9, part 2

--- 2022/day9/Program.fs
+++ 2022/day9/Program.fs
@@ -3,7 +3,18 @@ let lines = File.ReadAllLines("input.txt")
 
 type Move = int*int*int*string
 type Position = int*int
-type Rope = { Head: Position; Tail: Position; }
+type Rope = {
+    Head: Position;
+    One: Position;
+    Two: Position;
+    Three: Position;
+    Four: Position;
+    Five: Position;
+    Six: Position;
+    Seven: Position;
+    Eight: Position;
+    Tail: Position;
+}
 
 let parseCommand (s:string) =
     let move = s[0]
@@ -22,51 +33,59 @@ let commands = lines |> List.ofArray |> List.map parseCommand
 // the series of moves for the head will mean adjustmenst to the tail
 // let's first figure out the sequence of coordinates that are zero based that the head will visit
 
-let rope = {Head = Position(0, 0); Tail = Position(0, 0)}
+let rope = {
+    Head = Position(0, 0);
+    One = Position(0, 0);
+    Two = Position(0, 0);
+    Three = Position(0, 0);
+    Four = Position(0, 0);
+    Five = Position(0, 0);
+    Six = Position(0, 0);
+    Seven = Position(0, 0);
+    Eight = Position(0, 0);
+    Tail = Position(0, 0)
+}
 
-let calculateTailPosition head tail =
-    let (headRow, headCol) = head
-    let (tailRow, tailCol) = tail
+let calculateKnotPosition leadingKnot followingKnot =
+    let (leadingRow, leadingCol) = leadingKnot
+    let (followingRow, followingCol) = followingKnot
 
-    let rowDiff = headRow - tailRow
-    let colDiff = headCol - tailCol
+    let rowDiff = leadingRow - followingRow
+    let colDiff = leadingCol - followingCol
 
     match (rowDiff,colDiff) with
-    | (0, 0) -> tail
-    | (0,-1) -> tail
-    | (0,-2) -> (headRow, tailCol - 1)
-    | (0, 1) -> tail
-    | (0, 2) -> (headRow, tailCol + 1)
-    | (-1, 0) -> tail
-    | (-2, 0) -> (tailRow - 1, headCol)
-    | (1, 0) -> tail
-    | (2, 0) -> (tailRow + 1, headCol)
+    | (0, 0) -> followingKnot
+    | (0,-1) -> followingKnot
+    | (0,-2) -> (leadingRow, followingCol - 1)
+    | (0, 1) -> followingKnot
+    | (0, 2) -> (leadingRow, followingCol + 1)
+    | (-1, 0) -> followingKnot
+    | (-2, 0) -> (followingRow - 1, leadingCol)
+    | (1, 0) -> followingKnot
+    | (2, 0) -> (followingRow + 1, leadingCol)
 
     // separated diagonally but not far enough
-    | (-1,-1) -> (tailRow, tailCol)
-    | (1, -1) -> (tailRow, tailCol)
-    | (-1, 1) -> (tailRow, tailCol)
-    | (1, 1) -> (tailRow, tailCol)
+    | (-1,-1) -> (followingRow, followingCol)
+    | (1, -1) -> (followingRow, followingCol)
+    | (-1, 1) -> (followingRow, followingCol)
+    | (1, 1) -> (followingRow, followingCol)
 
     // diagonals
-    | (2, -1) -> (tailRow + 1, headCol)
-    | (2,  1) -> (tailRow + 1, headCol)
-    | (1, -2) -> (headRow, tailCol - 1)
-    | (1,  2) -> (headRow, tailCol + 1)
-    | (-1, -2) -> (headRow, tailCol - 1)
-    | (-1,  2) -> (headRow, tailCol + 1)
-    | (-2, -1) -> (tailRow - 1, headCol)
-    | (-2,  1) -> (tailRow - 1, headCol)
+    | (2, -1) -> (followingRow + 1, leadingCol)
+    | (2,  1) -> (followingRow + 1, leadingCol)
+    | (1, -2) -> (leadingRow, followingCol - 1)
+    | (1,  2) -> (leadingRow, followingCol + 1)
+    | (-1, -2) -> (leadingRow, followingCol - 1)
+    | (-1,  2) -> (leadingRow, followingCol + 1)
+    | (-2, -1) -> (followingRow - 1, leadingCol)
+    | (-2,  1) -> (followingRow - 1, leadingCol)
+    | (2, -2) -> (followingRow + 1, followingCol - 1)
+    | (2,  2) -> (followingRow + 1, followingCol + 1)
+    | (-2, -2) -> (followingRow - 1, followingCol - 1)
+    | (-2,  2) -> (followingRow - 1, followingCol + 1)
 
     | _ -> failwith $"Invalid tail position: {rowDiff}, {colDiff}"
 
-let getTail rope =
-    let {Head = _; Tail = tail} = rope
-    tail
-
-let getHead rope =
-    let {Head = head; Tail = _} = rope
-    head
 
 let rec stepExecution (acc: Rope list) move =
     let (rowAdj, colAdj, magnitude, desc) = move
@@ -74,17 +93,43 @@ let rec stepExecution (acc: Rope list) move =
     if magnitude = 0 then
         acc
     else
-        let (headRow, headCol) = acc |> List.last |> getHead
-        let tail = acc |> List.last |> getTail
-        
-        let newHead = Position(headRow + rowAdj, headCol + colAdj)
-        let newTail = calculateTailPosition newHead tail
+        let lastRope = acc |> List.last
 
-        printfn "   head: %A; tail: %A" newHead newTail
+        let (headRow, headCol) = lastRope.Head
+        let newHead = Position(headRow + rowAdj, headCol + colAdj)
+        
+        let one = lastRope.One
+        let newOne = calculateKnotPosition newHead one
+
+        let two = lastRope.Two
+        let newTwo = calculateKnotPosition newOne two
+
+        let three = lastRope.Three
+        let newThree = calculateKnotPosition newTwo three
+
+        let four = lastRope.Four
+        let newFour = calculateKnotPosition newThree four
+
+        let five = lastRope.Five
+        let newFive = calculateKnotPosition newFour five
+
+        let six = lastRope.Six
+        let newSix = calculateKnotPosition newFive six
+
+        let seven = lastRope.Seven
+        let newSeven = calculateKnotPosition newSix seven
+
+        let eight = lastRope.Eight
+        let newEight = calculateKnotPosition newSeven eight
+
+        let tail = lastRope.Tail
+        let newTail = calculateKnotPosition newEight tail
 
         let newMove = (rowAdj, colAdj, magnitude - 1, desc)
 
-        newMove |> stepExecution (acc @ [{Head = newHead; Tail = newTail}])
+        let newRope = {Head = newHead; One = newOne; Two = newTwo; Three = newThree; Four = newFour; Five = newFive; Six = newSix; Seven = newSeven; Eight = newEight; Tail = newTail}
+
+        newMove |> stepExecution (acc @ [newRope])
 
 
 printfn "moving..."
@@ -95,4 +140,4 @@ let sequence =
 
 // count of unique positions
 printfn "count: %d" (sequence |> List.length)
-printfn "unique: %d" (sequence |> List.map getTail |> List.distinct |> List.length)
+printfn "unique: %d" (sequence |> List.map (fun r -> r.Tail) |> List.distinct |> List.length)
