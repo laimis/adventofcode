7b5e8d0a304c3046ed4cfd82328e9ec8d50e07fc
unknown <laimis@gmail.com>
2022-12-09 11:36:32 -0800

    increase readability

--- 2022/day9/Program.fs
+++ 2022/day9/Program.fs
@@ -1,12 +1,19 @@
 ﻿open System.IO
 let lines = File.ReadAllLines("input.txt")
 
-type Move = int*int*int*string
+type Move = 
+    {rowAdj: int; colAdj: int; magnitude: int; direction: string}
+    member this.decrementMagnitude() =
+        let {rowAdj = rowAdj; colAdj = colAdj; magnitude = magnitude; direction = direction} = this
+
+        match magnitude with
+        | 0 -> failwith "Move cannot be decremented any further"
+        | _ -> {rowAdj = rowAdj; colAdj = colAdj; magnitude = magnitude - 1; direction = direction}
 
 type Position = 
     {row: int; col: int}
     member this.apply (move: Move) =
-        let (rowAdj, colAdj, _, _) = move
+        let {rowAdj = rowAdj; colAdj = colAdj; magnitude = magnitude; direction = direction} = move
         let {row = row;  col = col} = this
         {row = row + rowAdj; col = col + colAdj}
     member this.subtract (other: Position) =
@@ -15,27 +22,20 @@ type Position =
         (row - otherRow, col - otherCol)
     static member create (row, col) = {row = row; col = col}
 
-type Rope = {
-    Head: Position;
-    One: Position;
-    Two: Position;
-    Three: Position;
-    Four: Position;
-    Five: Position;
-    Six: Position;
-    Seven: Position;
-    Eight: Position;
-    Tail: Position;
-}
+type Rope =
+    { Parts : Position list}
+    static member create(number) = 
+        let parts = List.init number (fun _ -> Position.create(0,0))
+        {Parts = parts}
 
 let parseCommand (s:string) =
     let move = s[0]
     let magnitude = int s[2..]
     match move with
-    | 'L' -> Move (0, -1, magnitude, "left")
-    | 'U' -> Move (1, 0, magnitude, "up")
-    | 'R' -> Move (0, 1, magnitude, "right")
-    | 'D' -> Move (-1, 0, magnitude, "down")
+    | 'L' -> {rowAdj = 0; colAdj = -1; magnitude = magnitude; direction = "left"}
+    | 'U' -> {rowAdj = 1; colAdj = 0; magnitude = magnitude; direction = "up"}
+    | 'R' -> {rowAdj = 0; colAdj = 1; magnitude = magnitude; direction = "right"}
+    | 'D' -> {rowAdj = -1; colAdj = 0; magnitude = magnitude; direction = "down"}
     | _ -> failwith "Invalid command"
 
 printfn "parsing..."
@@ -45,18 +45,7 @@ let commands = lines |> List.ofArray |> List.map parseCommand
 // the series of moves for the head will mean adjustmenst to the tail
 // let's first figure out the sequence of coordinates that are zero based that the head will visit
 
-let rope = {
-    Head = Position.create(0, 0);
-    One = Position.create(0, 0);
-    Two = Position.create(0, 0);
-    Three = Position.create(0, 0);
-    Four = Position.create(0, 0);
-    Five = Position.create(0, 0);
-    Six = Position.create(0, 0);
-    Seven = Position.create(0, 0);
-    Eight = Position.create(0, 0);
-    Tail = Position.create(0, 0);
-}
+let rope = Rope.create(10)
 
 let calculateKnotPosition leadingKnot followingKnot =
     let {row = leadingRow;  col = leadingCol} = leadingKnot
@@ -99,45 +88,30 @@ let calculateKnotPosition leadingKnot followingKnot =
 
 
 let rec stepExecution (acc: Rope list) move =
-    let (rowAdj, colAdj, magnitude, desc) = move
+    let {magnitude = magnitude} = move
 
     if magnitude = 0 then
         acc
     else
         let rope = acc |> List.last
 
-        let newHead = rope.Head.apply move
-        
-        let one = rope.One
-        let newOne = calculateKnotPosition newHead one
+        let {Parts = parts } = rope
 
-        let two = rope.Two
-        let newTwo = calculateKnotPosition newOne two
+        let folder (acc: Position list) (part: Position) =
+            match acc with
+            | [] -> 
+                let newPart = part.apply move
+                acc @ [newPart]
+            | _ ->
+                let leadingKnot = acc |> List.last
+                let newPart = calculateKnotPosition leadingKnot part
+                acc @ [newPart]
 
-        let three = rope.Three
-        let newThree = calculateKnotPosition newTwo three
+        let newParts = parts |> List.fold folder []
 
-        let four = rope.Four
-        let newFour = calculateKnotPosition newThree four
+        let newMove = move.decrementMagnitude()
 
-        let five = rope.Five
-        let newFive = calculateKnotPosition newFour five
-
-        let six = rope.Six
-        let newSix = calculateKnotPosition newFive six
-
-        let seven = rope.Seven
-        let newSeven = calculateKnotPosition newSix seven
-
-        let eight = rope.Eight
-        let newEight = calculateKnotPosition newSeven eight
-
-        let tail = rope.Tail
-        let newTail = calculateKnotPosition newEight tail
-
-        let newMove = (rowAdj, colAdj, magnitude - 1, desc)
-
-        let newRope = {Head = newHead; One = newOne; Two = newTwo; Three = newThree; Four = newFour; Five = newFive; Six = newSix; Seven = newSeven; Eight = newEight; Tail = newTail}
+        let newRope = {Parts = newParts}
 
         newMove |> stepExecution (acc @ [newRope])
 
@@ -150,4 +124,4 @@ let sequence =
 
 // count of unique positions
 printfn "count: %d" (sequence |> List.length)
-printfn "unique: %d" (sequence |> List.map (fun r -> r.Tail) |> List.distinct |> List.length)
+printfn "unique: %d" (sequence |> List.map (fun r -> r.Parts |> List.last) |> List.distinct |> List.length)
