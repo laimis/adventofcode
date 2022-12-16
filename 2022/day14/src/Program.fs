078a73adc2e4568d2cc7fb3d90562e5c8abdfd84
unknown <laimis@gmail.com>
2022-12-16 12:08:25 -0800

    day 14, part 1, before answer

--- 2022/day14/src/Program.fs
+++ 2022/day14/src/Program.fs
@@ -1,4 +1,4 @@
-﻿namespace Day14
+﻿namespace AdventOfCode
 
 module Day14 =
 
@@ -7,5 +7,95 @@ module Day14 =
         y : int
     }
 
-    // For more information see https://aka.ms/fsharp-console-apps
-    printfn "Hello from F#"
+    type Line = {
+        start: Point
+        finish: Point
+    }
+
+    let parsePoint (input:string) =
+        let parts = input.Split(',')
+        match parts with
+        | [|x; y|] -> {x = int x; y = int y}
+        | _ -> failwith $"unexpected point format {input}"
+
+    let parseLines (input:string) =
+        input.Split(" -> ")
+        |> Array.pairwise
+        |> Array.map( fun (s,e) -> {start=parsePoint s; finish=parsePoint e})
+
+    let extractPoints (line:Line) =
+        
+        let startX = line.start.x
+        let startY = line.start.y
+        let finishX = line.finish.x
+        let finishY = line.finish.y
+
+        match (startX, startY, finishX, finishY) with
+        | (sx,_,fx,_) when sx = fx -> 
+           let min = System.Math.Min(startY, finishY)
+           let max = System.Math.Max(startY, finishY)
+           
+           [|min..max|]
+           |> Array.map( fun py -> {x = startX; y = py})
+
+        | (_, sy, _, fy) when sy = fy ->
+            let min = System.Math.Min(startX, finishX)
+            let max = System.Math.Max(startX, finishX)
+            
+            [|min..max|]
+            |> Array.map( fun px -> {x = px; y = startY})
+
+        | _ -> failwith "unexpected line configuration"
+
+    let parseToDistinctPoints (input:string) =
+        input.Split(System.Environment.NewLine)
+        |> Array.collect parseLines
+        |> Array.collect extractPoints
+        |> Array.distinct
+
+    let findHeight (points:Point array) =
+        (points |> Array.maxBy (fun p -> p.y)).y
+
+    let findWidth (points:Point array) =
+        (
+            (points |> Array.minBy (fun p -> p.x)).x,
+            (points |> Array.maxBy (fun p -> p.x)).x
+        )
+
+    let generateString (points:Point array) =
+        let height = points |> findHeight
+        let (minx, maxx) = points |> findWidth
+
+        let width = maxx - minx + 1
+
+        let str =
+            String.init
+                (width * height + height + 1)
+                (fun index ->
+                    
+                    let row = index / (height + 1)
+                    let column = index - row * width
+
+                    let candidatePoint = {x = minx + column; y = row}
+
+                    let contains = points |> Array.contains candidatePoint
+
+                    match contains with
+                    | true -> "#"
+                    | false -> "."
+                )
+
+        let rows = 
+            str
+            |> Seq.chunkBySize width
+            |> Seq.map (fun c -> System.String(c))
+
+        String.concat "" rows
+
+    let points =
+        System.IO.File.ReadAllText("input.txt")
+        |> parseToDistinctPoints
+
+    System.Console.WriteLine(points |> generateString)
+
+    
--- 2022/day14/test/Tests.fs
+++ 2022/day14/test/Tests.fs
@@ -1,9 +1,99 @@
 module Tests
 
-open System
 open Xunit
+open AdventOfCode.Day14
+
+let _sampleLines = @"498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9"
 
 [<Fact>]
-let ``My test`` () =
-    let point : Day14.Day14.Point = {x=1; y=2}
-    Assert.True(point.x = 1)
+let ``Point parsing works`` () =
+    let point = parsePoint "498,13"
+
+    Assert.True(498 = point.x)
+    Assert.True(13 = point.y)
+
+[<Fact>]
+let ``Multiple points are parsed correctly`` () =
+    let input = "498,13 -> 498,17 -> 491,17 -> 491,21 -> 507,21 -> 507,17 -> 502,17 -> 502,13"
+
+    let points =
+        input.Split(" -> ")
+        |> Array.map parsePoint
+
+    Assert.Equal(8, points.Length)
+
+    let firstPoint = points[0]
+    Assert.Equal(498, firstPoint.x)
+    Assert.Equal(13, firstPoint.y)
+
+    let lastPoint = points[points.Length - 1]
+    Assert.Equal(502, lastPoint.x)
+    Assert.Equal(13, lastPoint.y)
+
+[<Fact>]
+let ``Line parsing works`` () =
+    let input = @"503,4 -> 502,4 -> 502,9 -> 494,9"
+
+    let lines = parseLines input
+
+    let firstLine = lines[0]
+
+    Assert.Equal(503, firstLine.start.x)
+    Assert.Equal(4, firstLine.start.y)
+    Assert.Equal(502, firstLine.finish.x)
+    Assert.Equal(4, firstLine.finish.y)
+
+[<Fact>]
+let ``Line to points works`` () =
+    let input = @"503,4 -> 502,4 -> 502,9 -> 494,9"
+
+    let firstLine = parseLines input |> Array.head
+
+    let points = extractPoints firstLine
+
+    Assert.Equal(2, points.Length)
+    Assert.Equal({x=502; y=4}, points[0])
+    Assert.Equal({x=503; y=4}, points[1])
+
+    let secondLine = parseLines input |> Array.skip 1 |> Array.head
+
+    let secondPoints = extractPoints secondLine
+
+    Assert.Equal(6, secondPoints.Length)
+
+    Assert.Equal({x=502; y=4}, secondPoints[0])
+    Assert.Equal({x=502; y=5}, secondPoints[1])
+    Assert.Equal({x=502; y=6}, secondPoints[2])
+    Assert.Equal({x=502; y=7}, secondPoints[3])
+    Assert.Equal({x=502; y=8}, secondPoints[4])
+    Assert.Equal({x=502; y=9}, secondPoints[5])
+
+[<Fact>]
+let ``Raw input to points sequence works`` () =
+    let points = parseToDistinctPoints _sampleLines
+    Assert.Equal(20, points.Length)
+
+[<Fact>]
+let ``Height discovery works`` () =
+
+    let height = _sampleLines |> parseToDistinctPoints |> findHeight
+
+    Assert.Equal(9, height)
+
+[<Fact>]
+let ``Width range discovery works`` () =
+
+    let (minX, maxX) = _sampleLines |>  parseToDistinctPoints |> findWidth
+
+    Assert.Equal(494, minX)
+    Assert.Equal(503, maxX)
+
+[<Fact>]
+let ``Render works`` () =
+
+    let output = _sampleLines |> parseToDistinctPoints |> generateString
+
+    let expected = @"............................................#...##....#...#...###...#.........#.........#.#########."
+
+    Assert.Equal(expected, output)
