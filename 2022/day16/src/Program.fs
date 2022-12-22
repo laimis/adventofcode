1bd656e82123977caad08485fa0241ca68f77883
unknown <laimis@gmail.com>
2022-12-22 08:08:58 -0800

    ability to calculate shortest distance

--- 2022/day16/src/Program.fs
+++ 2022/day16/src/Program.fs
@@ -15,11 +15,7 @@
                 )
 
             let m = regex.Match(line)
-            System.Console.WriteLine(regex.IsMatch(line))
             
-            for g in m.Groups do
-                System.Console.WriteLine(g.Value)
-
             {
                 rate = int (m.Groups[2].Value);
                 next = m.Groups[3].Value.Split(", ") |> List.ofSeq;
@@ -46,4 +42,76 @@ Valve HH has flow rate=22; tunnel leads to valve GG
 Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II"
 
-let valves = Day16.parseValves lines
+
+open Day16
+
+let bfs verbose (tree:Map<string,Valve>) start =
+    
+    let visit (currentNode:string) =
+        if verbose then
+            System.Console.WriteLine(currentNode)
+
+    let queue = new System.Collections.Generic.Queue<string>()
+
+    queue.Enqueue(start)
+
+    let visited = new System.Collections.Generic.HashSet<string>()
+    visited.Add(start) |> ignore
+
+    while queue.Count > 0 do
+        let currentNode = queue.Dequeue()
+
+        visit currentNode
+
+        visited.Add(currentNode) |> ignore
+
+        let valve = tree[currentNode]
+
+        valve.next
+        |> List.filter ( fun n -> visited.Contains(n) |> not)
+        |> List.iter (queue.Enqueue)
+
+let numberOfSteps verbose (tree:Map<string,Valve>) start ending =
+    
+    let maxDistance = 1000
+    let rec numberOfStepsRec currentNode visited distance =
+        if verbose then
+            System.Console.Write($"search for {ending}, at {currentNode} with distance {distance}")
+
+        if currentNode = ending then
+            if verbose then
+                System.Console.WriteLine(", found!")
+                System.Console.ReadLine() |> ignore
+            Some distance
+        else
+            if verbose then
+                    System.Console.WriteLine(", not found...")
+                    System.Console.ReadLine() |> ignore
+            let valve = tree[currentNode]
+            let candidateDistances =
+                valve.next
+                |> List.filter (fun n -> visited |> Set.contains n |> not)
+                |> List.map (fun n -> numberOfStepsRec n (visited |> Set.add currentNode) (distance + 1))
+                |> List.choose id
+
+            match candidateDistances with
+            | [] -> None
+            | _ -> Some (candidateDistances |> List.min)
+
+    let distance = numberOfStepsRec start Set.empty<string> 0
+    if verbose then
+        System.Console.WriteLine($"distance from {start} to {ending} is {distance}")
+    distance
+
+let valves = parseValves lines
+
+let verbose = true
+
+bfs verbose valves "AA"
+
+for from in valves.Keys do
+    for toNode in valves.Keys do
+        let distance = numberOfSteps false valves from toNode
+        System.Console.WriteLine($"Distance from {from} to {toNode} is {distance}")
+
+
